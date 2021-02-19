a2d0d7b9ebdd2083c464a457cd70222b2381d32e
Andre O'Brien <andre.obrien877@gmail.com>
2021-02-19 17:30:06 +1300

    create PerformanceData model, migration and route for main

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import PerformanceData
+
 # Register your models here.
+admin.site.register(PerformanceData)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class PerformanceData(models.Model):
+    page_name = models.CharField(max_length=200)
+    data = models.TextField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return str(self.pub_date) + ' (' + self.page_name + ')'

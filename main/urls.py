ad16effcc2e7a49a82a1d3e44df21cda92f46da2
Andre O'Brien <andre.obrien877@gmail.com>
2021-02-23 11:06:42 +1300

    update views and routes

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:performance_data_id>/', views.detail, name='detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,17 @@
+import json
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import PerformanceData
+
 def index(request):
-    return HttpResponse("Hello, world")
+    items = PerformanceData.objects.order_by('-pub_date')[:50]
+    return render(request, 'main/index.html', {'items': items})
+
+def detail(request, performance_data_id):
+    item = get_object_or_404(PerformanceData, pk=performance_data_id)
+    data = json.loads(item.data)['log']
+    page = data['pages'][0]['title']
+    return render(request, 'main/detail.html', {'item': item, 'page': page, 'entries': data['entries']})
